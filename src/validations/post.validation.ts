3656720dcaa38af10a64cefa6c44ca27c05a6590
Luis <Luistejeda0011@gmail.com>
2022-11-25 00:00:23 -0400

    Adding post validation

---
+++ src/validations/post.validation.ts
@@ -0,0 +1,48 @@
+import { postType, NewPostEntry } from '../models/post.model'
+import { isString, isNumber, isPostType, notEmpty } from '../utils/utils'
+
+const parsePostType = (PostTypeFromRequest: any): postType => {
+  if (!isString(PostTypeFromRequest) || !isPostType(PostTypeFromRequest) || notEmpty(PostTypeFromRequest)) {
+    throw new Error('Invalid Post Type')
+  }
+  return PostTypeFromRequest
+}
+
+const parseUserID = (userIDFromRequest: any): number => {
+  if (!isNumber(userIDFromRequest) || notEmpty(userIDFromRequest)) {
+    throw new Error('Invalid User ID')
+  }
+  return userIDFromRequest
+}
+
+const parseLocation = (LocationFromRequest: any): string => {
+  if (!isString(LocationFromRequest) || notEmpty(LocationFromRequest)) {
+    throw new Error('Invalid User ID')
+  }
+  return LocationFromRequest
+}
+
+const parseImg = (imgFromRequest: any): string => {
+  if (!isString(imgFromRequest) || notEmpty(imgFromRequest)) {
+    throw new Error('Invalid Image')
+  }
+  return imgFromRequest
+}
+const parseDescription = (descriptionFromRequest: any): string => {
+  if (!isString(descriptionFromRequest) || notEmpty(descriptionFromRequest)) {
+    throw new Error('Invalid Description')
+  }
+  return descriptionFromRequest
+}
+
+export const toNewPost = (object: any, userIdFromRequest: any): NewPostEntry => {
+  const newPost: NewPostEntry = {
+    Post_Type: parsePostType(object.Post_Type),
+    User_ID: parseUserID(userIdFromRequest),
+    Location: parseLocation(object.Location),
+    img: parseImg(object.img),
+    img_ID: parseImg(object.img_ID),
+    description: parseDescription(object.description)
+  }
+  return newPost
+}
