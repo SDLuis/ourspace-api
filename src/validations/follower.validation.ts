6245a96b77968f8c4d8845e90881754a70a37bb0
Luis <Luistejeda0011@gmail.com>
2023-01-10 17:10:18 -0400

    Follower validation added

---
+++ src/validations/follower.validation.ts
@@ -0,0 +1,24 @@
+import { NewFollowerEntry } from '../models/follower.model'
+import { notEmpty, isNumber } from '../utils/utils'
+
+const parseUserID = (userIDFromRequest: any): number => {
+  if (!isNumber(userIDFromRequest) || notEmpty(userIDFromRequest)) {
+    throw new Error('Invalid User ID')
+  }
+  return userIDFromRequest
+}
+const parseFollowerID = (followerIDFromRequest: any): number => {
+  if (!isNumber(followerIDFromRequest) || notEmpty(followerIDFromRequest)) {
+    throw new Error('Invalid User ID')
+  }
+  return followerIDFromRequest
+}
+
+export const toNewReaction = (followerIDFromRequest: any, userIdFromRequest: any): NewFollowerEntry => {
+  const newReaction: NewFollowerEntry = {
+    User_ID: parseUserID(userIdFromRequest),
+    Follower_ID: parseFollowerID(followerIDFromRequest)
+
+  }
+  return newReaction
+}
