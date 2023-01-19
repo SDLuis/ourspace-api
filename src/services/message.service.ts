7aaea249c82761e93b832115ac952c948e3f8b67
Luis <Luistejeda0011@gmail.com>
2023-01-19 19:27:04 -0400

    Message services added

---
+++ src/services/message.service.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions, @typescript-eslint/no-floating-promises */
+import { userModel } from '../models/user.model'
+import '../models/index'
+import { messageModel, messageEntry, NewMessageEntry } from '../models/message.model'
+
+export const getMessages = async (): Promise<messageEntry[]> => {
+  return messageModel.findAll({
+    include: { model: userModel, attributes: { exclude: ['password'] } },
+    order: [
+      ['createdAt', 'DESC']
+    ]
+  })
+    .then((result) => {
+      return result
+    }) as any
+}
+
+export const addNewMessage = async (NewMessageEntry: NewMessageEntry): Promise<NewMessageEntry> => {
+  const newMessage = {
+    ...NewMessageEntry
+  }
+  const message = await messageModel.create(newMessage)
+  return message
+}
+
+export const deleteMessage = (id: number): Promise<number> | undefined => {
+  return messageModel.destroy({ where: { Message_ID: id } }) as any
+}
+
+export const getMessagesByConversation = async (conversationId: number): Promise<messageEntry[] | undefined> => {
+  return messageModel.findAll({ where: { Conversation_ID: conversationId }, include: { model: userModel, attributes: { exclude: ['password'] } }, order: ['Conversation_ID', 'DESC'] }) as any
+}
