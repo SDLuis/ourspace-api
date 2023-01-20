ee0812588b5d18b7cfdac74628cb2889dffbb4a5
Luis <Luistejeda0011@gmail.com>
2023-01-20 15:32:20 -0400

    Message controller added

---
+++ src/controllers/message.controller.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express'
+import * as messageService from '../services/message.service'
+
+export const newMessage = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const body = {
+      Sender_ID: (req as any).token.User_ID,
+      description: req.body.secondUser
+    }
+    const addedConversation = await messageService.addNewMessage(body)
+    res.status(200).send(addedConversation)
+  } catch (err: any) {
+    res.status(400).send(err.message)
+  }
+}
+
+export const deleteMessage = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const id = +req.params.messageID
+    await messageService.deleteMessage(id)?.then((result) => {
+      if (result === 1) {
+        res.status(200).send('Message deleted')
+      } else {
+        res.status(400).send('Error, Message was not edited')
+      }
+    })
+  } catch (e: any) {
+    res.status(400).send(e.message)
+  }
+}
+
+export const getMessagesByConversations = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const id = req.body.conversationID
+    const Messages = (await messageService.getMessagesByConversation(id)) as any
+    res.status(200).send(Messages)
+  } catch (e: any) {
+    res.status(400).send(e.message)
+  }
+}
